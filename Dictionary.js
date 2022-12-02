d97af593036774f9c29b615ee99b490b4d8dd9be
AndrewYoungson <114414403+AndrewYoungson@users.noreply.github.com>
2022-12-02 10:34:48 +0000

    created dictionary javascript
    made the dictionary javascript which will allow api to link with main page

---
+++ Dictionary.js
@@ -0,0 +1,12 @@
+function getData(){
+let Word =document.getElementById("searchWord").value0
+l.et url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+  
+  fetch(url)
+  .then((resp) => resp.json
+        
+  .then(function(data) {
+    
+  let wordEl = document.getelemt
+  
+  })
